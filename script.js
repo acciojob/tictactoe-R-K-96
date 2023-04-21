3acdfe3e1fb58c7aa18be82163e17fb9056f22e5
R-K-96 <124651427+R-K-96@users.noreply.github.com>
2023-04-21 10:48:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,130 +1,168 @@
-//your JS code here. If required.
+// Start Game events and data changes
+const button = document.querySelectorAll('div>button');
+var player1, player2;
+button[0].addEventListener('click', submitFunc);
 
-const inputContainer = document.getElementById("input-container");
-const gameContainer = document.getElementById("game-container");
-const player1 = document.getElementById("player1");
-const player2 = document.getElementById("player2");
-
-const message = document.querySelector("#message");
-const error = document.getElementById("error");
-
-const startGame = document.getElementById("submit");
-const restartbtn = document.getElementById("restartBtn");
-
-const cells = document.querySelectorAll(".cell");
-
-const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-];
-let options = ["", "", "", "", "", "", "", "", ""];
-
-let currentPlayer;
-let running = false;
-
-
-
-restartbtn.addEventListener("click", restart);
-startGame.addEventListener("click", start);
-
-function start() {
-    if(player1.value==="" ||player2.value==="")
-    {
-        console.log("enter name");
-
-        error.innerText =" Enter Player's Name";
-
-    }
-    else{
-
-        inputContainer.style.display = "none";
-    gameContainer.style.display = "block";
-
-    initializeGame();
-    }
+function submitFunc() {
+    const player = document.querySelectorAll('div>input');
     
-    
-}
+    player1 = player[0].value;
+    player2 = player[1].value;
+    if(player1&&player2) {
+        const inputCont = document.getElementById("input-cont");
+        const gridCont = document.getElementById("grid-cont");
+        inputCont.style.display = 'none';
+        gridCont.style.display = 'flex';
 
-function initializeGame(){
-    cells.forEach(cell => cell.addEventListener("click", cellClicked));
-
-    player1Name =player1.value;
-    player2Name = player2.value;
-    currentPlayer = player1Name;
-   
-    message.textContent = `${currentPlayer}'s turn`;
-    running = true;
-}
-
-function cellClicked(){
-    const cellIndex = this.getAttribute("id");
-
-    if(options[cellIndex] != "" || !running){
-        return;
-    }
-
-    updateCell(this, cellIndex);
-    checkWinner();
-}
-function updateCell(cell, index){
-    options[index] = currentPlayer;
-    if(currentPlayer==player1Name)
-    {
-        cell.textContent = "X";
+        gameStart();
     }
     else {
-        cell.textContent = "O";
+        alert('Please enter both the player names');
+    }
+}
+
+function gameStart() {
+    const subHeading = document.getElementsByClassName('message');
+    
+    subHeading[0].innerText = player1+", you're up";
+}
+
+var turn = 0;
+var gameOn = true;
+var count = 0;
+
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+
+one.addEventListener('click', placeYourPick);
+two.addEventListener('click', placeYourPick);
+three.addEventListener('click', placeYourPick);
+four.addEventListener('click', placeYourPick);
+five.addEventListener('click', placeYourPick);
+six.addEventListener('click', placeYourPick);
+seven.addEventListener('click', placeYourPick);
+eight.addEventListener('click', placeYourPick);
+nine.addEventListener('click', placeYourPick);
+
+
+function placeYourPick(event) {
+    if(count!=9&&gameOn) {
+        let playerMark = 'o';
+        if(turn==0) {
+            playerMark = 'x'
+        }
+        event.srcElement.innerText = playerMark;
+        count++;
+		const subHeading = document.getElementsByClassName('message');
+        let won = checkForWin();
+        if(won) {
+            gameOn = false;            
+            if(turn) {
+                subHeading[0].innerText = player2+" congratulations you won!";
+            }
+            else {
+                subHeading[0].innerText = player1+" congratulations you won!";           
+            }
+        }
+		else {
+			
+			if(turn) {
+				subHeading[0].innerText = player1+", you're up";
+			}
+			else subHeading[0].innerText = player2+", you're up";
+		}
+        if(turn) {
+            turn = 0;
+        }
+        else turn++;
+    }
+    else {
+        const subHeading = document.getElementsByClassName('message');
+        subHeading[0].innerText = "Game Completed, click on restart for a new game";        
+    }
+}
+function checkForWin() {
+    if(one.innerText=='x'&&two.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&four.innerText=='x'&&seven.innerText=='x') {
+        return true;
+    }
+    if(four.innerText=='x'&&five.innerText=='x'&&six.innerText=='x') {
+        return true;
+    }
+    if(seven.innerText=='x'&&eight.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(five.innerText=='x'&&two.innerText=='x'&&eight.innerText=='x') {
+        return true;
+    }
+    if(six.innerText=='x'&&nine.innerText=='x'&&three.innerText=='x') {
+        return true;
+    }
+    if(one.innerText=='x'&&five.innerText=='x'&&nine.innerText=='x') {
+        return true;
+    }
+    if(three.innerText=='x'&&five.innerText=='x'&&seven.innerText=='x') {
+        return true;
     }
     
-}
-function changePlayer(){
-    currentPlayer = (currentPlayer == player1Name) ? player2Name : player1Name;
-    message.textContent = `${currentPlayer},you're up`;
-}
-function checkWinner(){
-    let roundWon = false;
-
-    for(let i = 0; i < winConditions.length; i++){
-        const condition = winConditions[i];
-        const cellA = options[condition[0]];
-        const cellB = options[condition[1]];
-        const cellC = options[condition[2]];
-
-        if(cellA == "" || cellB == "" || cellC == ""){
-            continue;
-        }
-        if(cellA == cellB && cellB == cellC){
-            roundWon = true;
-            break;
-        }
+    if(one.innerText=='o'&&two.innerText=='o'&&three.innerText=='o') {
+        return true;
     }
-
-    if(roundWon){
-        message.textContent = `${currentPlayer} congratulations you won!`;
-        running = false;
+    if(one.innerText=='o'&&four.innerText=='o'&&seven.innerText=='o') {
+        return true;
     }
-    else if(!options.includes("")){
-        message.textContent = `Draw!`;
-        running = false;
+    if(four.innerText=='o'&&five.innerText=='o'&&six.innerText=='o') {
+        return true;
     }
-    else{
-        changePlayer();
+    if(seven.innerText=='o'&&eight.innerText=='o'&&nine.innerText=='o') {
+        return true;
     }
+    if(five.innerText=='o'&&two.innerText=='o'&&eight.innerText=='o') {
+        return true;
+    }
+    if(six.innerText=='o'&&nine.innerText=='o'&&three.innerText=='o') {
+        return true;
+    }
+    if(one.innerText=='o'&&five.innerText=='o'&&nine.innerText=='o') {
+        return true;
+    }
+    if(three.innerText=='o'&&five.innerText=='o'&&seven.innerText=='o') {
+        return true;
+    }
+    return false;
 }
 
+button[1].addEventListener('click', clearBoard);
+button[2].addEventListener('click',restartGame);
 
-function restart() {
-    
-    inputContainer.style.display = "inline-flex";
-    gameContainer.style.display = "none";
-    error.innerText = "";
-    player1.value = "";
-    player2.value = "";    
+function clearBoard() {
+    count = 0;
+    one.innerText = '';
+    two.innerText = '';
+    three.innerText = '';
+    four.innerText = '';
+    five.innerText = '';
+    six.innerText = '';
+    seven.innerText = '';
+    eight.innerText = '';
+    nine.innerText = '';
+    turn = 0;
+    gameOn = true;
+}
+
+function restartGame() {
+    const inputCont = document.getElementById("input-cont");
+    const gridCont = document.getElementById("grid-cont");
+    inputCont.style.display = 'block';
+    gridCont.style.display = 'none';
+    clearBoard();
 }
